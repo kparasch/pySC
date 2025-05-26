d1c9e233da8ca7c71226090f5f8700a09396056c
kparasch <konstantinos.paraschou@desy.de>
2025-05-26 12:41:28 +0200

    made calculation of bpm reading more sane

--- pySC/core/bpm_system.py
+++ pySC/core/bpm_system.py
@@ -2,10 +2,15 @@ import numpy as np
 from .beam import bpm_reading
 from ..utils import at_wrapper
 
+def _rotation_matrix(a):
+    return np.array([[np.cos(a), -np.sin(a)], [np.sin(a), np.cos(a)]])
+
 class BPMSystem:
     def __init__(self, SC, dual_plane=True):
         if not dual_plane:
             raise NotImplementedError('All BPMs should be dual plane')
+
+        self.rng = np.random.default_rng()
 
         self.SC = SC
         self.indices = SC.ORD.BPM
@@ -32,13 +37,28 @@ class BPMSystem:
         self.support_offsets_y = np.array([SC.RING[index].SupportOffset[1] for index in self.indices])
         self.support_rolls = np.array([SC.RING[index].SupportRoll for index in self.indices])
 
-    def _capture_orbit(self):
-        orbit = at_wrapper.get_orbit(self.SC.RING)
-        self.SC.INJ.trackMode = 'ORB'
-        orbit, _ = bpm_reading(self.SC)
-        return orbit
-    
+        self.update_offset_and_support()
+
+    def update_offset_and_support(self):
+        self.total_offsets_x = self.offsets_x + self.support_offsets_x
+        self.total_offsets_y = self.offsets_y + self.support_offsets_y
+        self.total_rolls = self.rolls + self.support_rolls
+        self.rot_matrices = _rotation_matrix(self.total_rolls)
+
     def capture_orbit(self):
+        orbit = at_wrapper.get_orbit(self.SC.RING, self.indices)
+        rotated_orbit = np.einsum('ijk,jk->ik', self.rot_matrices, orbit)  # Rotate orbit according to bpm roll
+
+        noise_x = self.rng.normal(scale=self.rms_errors['noise_co_x'], size=orbit[0].shape)
+        noise_y = self.rng.normal(scale=self.rms_errors['noise_co_y'], size=orbit[1].shape)
+
+        fake_orbit_x = (rotated_orbit[0] - self.total_offsets_x) * (1 + self.calibration_errors_x) + noise_x
+        fake_orbit_y = (rotated_orbit[1] - self.total_offsets_y) * (1 + self.calibration_errors_y) + noise_y
+
+        return np.array([fake_orbit_x, fake_orbit_y])
+
+    
+    def capture_orbit_old(self):
         self.SC.INJ.trackMode = 'ORB'
         orbit, _ = bpm_reading(self.SC)
         return orbit

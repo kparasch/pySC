e7ff0f462637012bec4f6bdbaa48bad471c97781
Elaf Musa <elaf.musa@desy.de>
2025-07-09 14:52:23 +0200

    elaf changes

--- pySC/correction/loco.py
+++ pySC/correction/loco.py
@@ -1,17 +1,20 @@
 import at
 import numpy as np
 import multiprocessing
-from pySC.lattice_properties.response_model import SCgetModelRM, SCgetModelDispersion
+from pySC.lattice_properties.response_model import SCgetModelRM_mod
 from pySC.core.constants import SETTING_ADD, TRACK_ORB
 from pySC.core.beam import bpm_reading
 import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
-from pySC.utils import logging_tools, sc_tools
+from pySC.utils import logging_tools, sc_tools, at_wrapper
+from pySC.lattice_properties.response_model import SCgetModelRING, orbpass
+
 LOGGER = logging_tools.get_logger(__name__)
 
 
 def calculate_jacobian(SC, C_model, dkick, used_cor_ind, bpm_indexes, quads_ind, dk, trackMode=TRACK_ORB,
-                       useIdealRing=True, skewness=False, order=1, method=SETTING_ADD, includeDispersion=False, rf_step=1E3,
+                       useIdealRing=True, skewness=False, order=1, method=SETTING_ADD, includeDispersion=False,
+                       rf_step=1E3,
                        cav_ords=None, full_jacobian=True):
     pool = multiprocessing.Pool()
     quad_args = [(quad_index, SC, C_model, dkick, used_cor_ind, bpm_indexes, dk, trackMode, useIdealRing,
@@ -30,7 +33,7 @@ def calculate_jacobian(SC, C_model, dkick, used_cor_ind, bpm_indexes, quads_ind,
         n_bpms = len(bpm_indexes) * 2  # in both planes
         j_cor = np.zeros((n_correctors,) + C_model.shape)
         for i in range(n_correctors):
-            j_cor[i, :, i] = C_model[:, i]   # a single column of response matrix corresponding to a corrector
+            j_cor[i, :, i] = C_model[:, i]  # a single column of response matrix corresponding to a corrector
         j_bpm = np.zeros((n_bpms,) + C_model.shape)
         for i in range(n_bpms):
             j_bpm[i, i, :] = C_model[i, :]  # a single row of response matrix corresponding to a given plane of BPM
@@ -50,16 +53,25 @@ def generating_quads_response_matrices(args):
         SC.set_magnet_setpoints(quad_index, -dk, skewness, order, method)
         return C_measured - C_model
 
-    dispersion_model = SCgetModelDispersion(SC, used_bpm_indexes, CAVords=cav_ords)
+    # dispersion_model = SCgetModelDispersion(SC, used_bpm_indexes, CAVords=cav_ords, rfStep=rf_step)
+    _, _, twiss = at.get_optics(SC.IDEALRING, used_bpm_indexes)  ## ADD dispersion to the ORMs from AT get_optics
+    dx = twiss.dispersion[:, 0]
+    dy = twiss.dispersion[:, 2]
+    dispersion_model = np.concatenate((dx, dy))
     SC.set_magnet_setpoints(quad_index, dk, skewness, order, method)
     C_measured = SCgetModelRM(SC, used_bpm_indexes, used_cor_indexes, dkick=correctors_kick, useIdealRing=useIdealRing,
                               trackMode=trackMode)
-    dispersion_meas = SCgetModelDispersion(SC, used_bpm_indexes, CAVords=cav_ords, rfStep=rf_step)
+    # dispersion_meas = SCgetModelDispersion(SC, used_bpm_indexes, CAVords=cav_ords, rfStep=rf_step, useIdealRing=False)
+    _, _, twiss = at.get_optics(SC.RING, used_bpm_indexes)
+    dx = twiss.dispersion[:, 0]
+    dy = twiss.dispersion[:, 2]
+    dispersion_meas = np.concatenate((dx, dy))
+    C_measured = np.hstack((C_measured, ((dispersion_meas - dispersion_model) / correctors_kick).reshape(-1, 1)))
     SC.set_magnet_setpoints(quad_index, -dk, skewness, order, method)
-    return np.hstack((C_measured - C_model, (dispersion_meas - dispersion_model).reshape(-1, 1)))
+    return C_measured - C_model
 
 
-def measure_closed_orbit_response_matrix(SC, bpm_ords, cm_ords, dkick=1e-5):
+def measure_closed_orbit_response_matrix(SC, bpm_ords, cm_ords, dkick=1e-5, includeDispersion=False):
     LOGGER.info('Calculating Measure response matrix')
     n_turns = 1
     n_bpms = len(bpm_ords)
@@ -76,28 +88,38 @@ def measure_closed_orbit_response_matrix(SC, bpm_ords, cm_ords, dkick=1e-5):
             SC.set_cm_setpoints(cm_ord, -dkick, skewness=bool(n_dim), method=SETTING_ADD)
             response_matrix[:, cnt] = np.ravel((closed_orbits1 - closed_orbits0) / dkick)
             cnt = cnt + 1
+    if includeDispersion == True:
+        _, _, twiss = at.get_optics(SC.RING, bpm_ords)
+        dx = twiss.dispersion[:, 0]
+        dy = twiss.dispersion[:, 2]
+        dispersion_meas = np.concatenate((dx, dy))
+        return np.hstack(
+            (response_matrix, ((dispersion_meas) / dkick).reshape(-1, 1)))
     return response_matrix
 
 
-def loco_correction_lm(initial_guess0, orm_model, orm_measured, Jn, lengths, including_fit_parameters, bounds=(-np.inf, np.inf), weights=1,
+def loco_correction_lm(initial_guess0, orm_model, orm_measured, Jn, lengths, including_fit_parameters,
+                       bounds=(-np.inf, np.inf), weights=1,
                        verbose=2):
-    mask = _get_parameters_mask(including_fit_parameters, lengths)
-    result = least_squares(lambda delta_params: objective(delta_params, orm_measured - orm_model, Jn[mask, :, :], weights),
-                           initial_guess0[mask], #bounds=bounds,
-                           method="lm",
-                           verbose=verbose)
+    mask = _get_parameters_mask2(including_fit_parameters, lengths)
+    result = least_squares(
+        lambda delta_params: objective(delta_params, orm_measured - orm_model, Jn[mask, :, :], weights),
+        initial_guess0[mask],  # bounds=bounds,
+        method="lm",
+        verbose=verbose)
     return result.x
 
 
-def loco_correction_ng(initial_guess0, orm_model, orm_measured, J, lengths, including_fit_parameters, s_cut, weights=1, LM_lambda=0):
+def loco_correction_ng(initial_guess0, orm_model, orm_measured, J, lengths, including_fit_parameters, s_cut, weights=1,
+                       ):
     initial_guess = initial_guess0.copy()
-    mask = _get_parameters_mask(including_fit_parameters, lengths)
+    mask = _get_parameters_mask2(including_fit_parameters, lengths)
     residuals = objective(initial_guess[mask], orm_measured - orm_model, J[mask, :, :], weights)
     r = residuals.reshape(np.transpose(orm_model).shape)
     t2 = np.zeros([len(initial_guess[mask]), 1])
     for i in range(len(initial_guess[mask])):
         t2[i] = np.sum(np.dot(np.dot(J[i].T, weights), r.T))
-    return get_inverse(J[mask, :, :], t2, s_cut, weights, LM_lambda=LM_lambda)
+    return get_inverse(J[mask, :, :], t2, s_cut, weights)
 
 
 def objective(masked_params, orm_residuals, masked_jacobian, weights):
@@ -114,7 +136,17 @@ def _get_parameters_mask(including_fit_parameters, lengths):
     return mask
 
 
-def set_correction(SC, r, elem_ind, individuals=True, skewness=False, order=1, method=SETTING_ADD, dipole_compensation=True):
+def _get_parameters_mask2(including_fit_parameters, lengths):
+    mask = np.zeros(sum(lengths), dtype=bool)
+    current_index = 0
+    for param, length in zip(including_fit_parameters, lengths):
+        mask[current_index:current_index + length] = True
+        current_index += length
+    return mask
+
+
+def set_correction(SC, r, elem_ind, individuals=True, skewness=False, order=1, method=SETTING_ADD,
+                   dipole_compensation=True):
     if individuals:
         SC.set_magnet_setpoints(elem_ind, -r, skewness, order, method, dipole_compensation=dipole_compensation)
         return SC
@@ -157,30 +189,137 @@ def select_equally_spaced_elements(total_elements, num_elements):
     return total_elements[::step]
 
 
-def get_inverse(jacobian, B, s_cut, weights, LM_lambda=0, plot=False):
-    """
-    Calculates $(J^t J + \lambda diag(J^t J))^{-1} J^t r_0$ using a modified 
-    version of the Levenberg-Marquardt method. More information at p.114 of
-    https://inis.iaea.org/collection/NCLCollectionStore/_Public/54/003/54003559.pdf
-
-    Args:
-        jacobian: jacobian matrix obtained from calculate_jacobian()
-        bpm_ords: array of element indices of registered BPMs for which to calculate readings
-            (for convenience, otherwise `SC.ORD.BPM` is used)
-        s_cut: number of kept singular values when inverting the matrix
-        weights: weights applied to the elements of the jacobian
-        LM_lambda: Levenberg-Marquardt lambda parameter. If LM_lambda=0, the 
-        function is equivalent to the Gauss-Newton method
-        plot: boolean flag to plot the resulting inverse matrix
-        
-    Returns:
-        Array of correction values of the same size as B.
-    """
+def get_inverse(jacobian, B, s_cut, weights, plot=False):
     n_resp_mats = len(jacobian)
-    Jt = np.sum(jacobian, axis=2)          # Sum over i and j for all planes
-    Jt_dot_J = np.dot(np.dot(Jt, weights), Jt.T)
-    matrix = Jt_dot_J + LM_lambda * np.diag(np.diag(Jt_dot_J))
+    sum_corr = np.sum(jacobian, axis=2)  # Sum over i and j for all planes
+    matrix = np.dot(np.dot(sum_corr, weights), sum_corr.T)
     inv_matrix = sc_tools.pinv(matrix, num_removed=n_resp_mats - min(n_resp_mats, s_cut), plot=plot)
     results = np.ravel(np.dot(inv_matrix, B))
     # e = np.ravel(np.dot(matrix, results)) - np.ravel(B)
     return results
+
+
+'''
+The code below is for optics calculations (based on AT get_optics) and generates plots for analyzing the lattice.
+ Example usage:
+
+ [-, -, twiss] = at.get_optics(SC.IDEALRING, SC.ORD.BPM)
+ analyze_ring(SC, twiss, SC.ORD.BPM, useIdealRing=False, makeplot=False)
+'''
+
+
+def analyze_ring(SC, twiss, bpm_indices, useIdealRing=True, makeplot=False):
+    ring = SC.IDEALRING.deepcopy() if useIdealRing else SC.RING  # SCgetModelRING(SC)
+    rmsx, rmsy = rms_orbits(ring, bpm_indices, SC)
+    bx_rms_err, by_rms_err = getBetaBeat(ring, twiss, bpm_indices)
+    dx_rms_err, dy_rms_err = getDispersionErr(ring, twiss, bpm_indices)
+
+    print(f"RMS horizontal orbit: {rmsx * 1.e6:.2f} mum, RMS vertical orbit: {rmsy * 1.e6:.2f} mum")
+    print(f"RMS horizontal beta beating: {bx_rms_err * 100:.2f}%, RMS vertical beta beating: {by_rms_err * 100:.2f}%")
+    print(
+        f"RMS relative horizontal dispersion: {dx_rms_err * 1000:.4f} mm, RMS relative vertical dispersion: {dy_rms_err*1000:.4f} mm")
+    print(f"Tune values: {at.get_tune(ring, get_integer=True)}, Chromaticity values: {at.get_chrom(ring)}")
+
+    if makeplot:
+        plot_orbits(ring, bpm_indices)
+        plot_beta_beat(ring, twiss, bpm_indices)
+        plot_dispersion_err(ring, twiss, bpm_indices)
+
+
+def calculate_rms(data):
+    return np.sqrt(np.mean(data ** 2))
+
+
+def plot_data(s_pos, data, xlabel, ylabel, title):
+    plt.rc('font', size=13)
+    fig, ax = plt.subplots(figsize=(6, 3))
+    ax.plot(s_pos, data)
+    ax.set_xlabel(xlabel, fontsize=14)
+    ax.set_ylabel(ylabel, fontsize=14)
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+    ax.grid(True, which='both', linestyle=':', color='gray')
+    plt.title(title)
+    plt.show()
+
+
+def rms_orbits(ring, elements_indexes,SC, trackMode='ORB', Z0=np.zeros(6)):
+    #track_methods = dict(TBT=at_wrapper.atpass, ORB=orbpass)
+    #if trackMode == 'ORB':
+    #    nTurns = 1
+    #trackmethod = track_methods['ORB']
+    #Ta = trackmethod(ring, Z0=Z0, nTurns=nTurns, REFPTS=elements_indexes)
+
+    #closed_orbitx = np.ravel(np.transpose(Ta[0, :, :, :], axes=(2, 1, 0)))
+    #closed_orbity = np.ravel(np.transpose(Ta[2, :, :, :], axes=(2, 1, 0)))
+
+    #[elemdata0, beamdata, elemdata] = at.get_optics(ring, elements_indexes)
+
+    #closed_orbitx = elemdata.closed_orbit[:, 0]
+    #closed_orbity = elemdata.closed_orbit[:, 2]
+
+    #rmsx = calculate_rms(closed_orbitx)
+    #rmsy = calculate_rms(closed_orbity)
+
+    trackMode = 'ORB'
+    T, _ = bpm_reading(SC)
+
+    closed_orbitx = T[0]
+    closed_orbity = T[1]
+
+    rmsx = np.sqrt(np.mean(closed_orbitx ** 2))
+    rmsy = np.sqrt(np.mean(closed_orbity ** 2))
+
+    return rmsx, rmsy
+
+
+def getBetaBeat(ring, twiss, elements_indexes):
+    _, _, twiss_error = at.get_optics(ring, elements_indexes)
+    bx = (twiss_error.beta[:, 0] - twiss.beta[:, 0]) / twiss.beta[:, 0]
+    by = (twiss_error.beta[:, 1] - twiss.beta[:, 1]) / twiss.beta[:, 1]
+
+    bx_rms = calculate_rms(bx)
+    by_rms = calculate_rms(by)
+
+    return bx_rms, by_rms
+
+
+def getDispersionErr(ring, twiss, elements_indexes):
+    _, _, twiss_error = at.get_optics(ring, elements_indexes)
+    dx = twiss_error.dispersion[:, 0] - twiss.dispersion[:, 0]
+    dy = twiss_error.dispersion[:, 2] - twiss.dispersion[:, 2]
+
+    dx_rms = calculate_rms(dx)
+    dy_rms = calculate_rms(dy)
+
+    return dx_rms, dy_rms
+
+
+def plot_orbits(ring, elements_indexes, trackMode='ORB', Z0=np.zeros(6)):
+    _, _, twiss = at.get_optics(ring, elements_indexes)
+    track_methods = dict(TBT=at_wrapper.lattice_track, ORB=orbpass)
+    if trackMode == 'ORB':
+        nTurns = 1
+    trackmethod = track_methods['ORB']
+    Ta = trackmethod(ring, Z0=Z0, nTurns=nTurns, REFPTS=elements_indexes)
+    closed_orbitx = np.ravel(np.transpose(Ta[0, :, :, :], axes=(2, 1, 0))) / 1.e-06
+    closed_orbity = np.ravel(np.transpose(Ta[2, :, :, :], axes=(2, 1, 0))) / 1.e-06
+    plot_data(twiss.s_pos, closed_orbitx, "s_pos [m]", r"closed_orbit x [$\mu$m]", "Horizontal closed orbit")
+    plot_data(twiss.s_pos, closed_orbity, "s_pos [m]", r"closed_orbit y [$\mu$m]", "Vertical closed orbit")
+
+
+def plot_beta_beat(ring, twiss, elements_indexes):
+    _, _, twiss_error = at.get_optics(ring, elements_indexes)
+    bx = (twiss_error.beta[:, 0] - twiss.beta[:, 0]) / twiss.beta[:, 0]
+    by = (twiss_error.beta[:, 1] - twiss.beta[:, 1]) / twiss.beta[:, 1]
+
+    plot_data(twiss_error.s_pos, bx, "s_pos [m]", r'$\Delta \beta_x / \beta_x$', "Horizontal beta beating")
+    plot_data(twiss_error.s_pos, by, "s_pos [m]", r'$\Delta \beta_y / \beta_y$', "Vertical beta beating")
+
+
+def plot_dispersion_err(ring, twiss, elements_indexes):
+    _, _, twiss_error = at.get_optics(ring, elements_indexes)
+    dx = twiss_error.dispersion[:, 0] - twiss.dispersion[:, 0]
+    dy = twiss_error.dispersion[:, 2] - twiss.dispersion[:, 2]
+
+    plot_data(twiss_error.s_pos, dx, "s_pos [m]", r'$\Delta \eta_x$ [m]', "Horizontal dispersion error")
+    plot_data(twiss_error.s_pos, dy, "s_pos [m]", r'$\Delta  \eta_y$ [m]', "Vertical dispersion error")
--- pySC/lattice_properties/response_model.py
+++ pySC/lattice_properties/response_model.py
@@ -1,6 +1,6 @@
 import copy
 import numpy as np
-from at import Lattice
+from at import Lattice, lattice_track
 
 from pySC.core.simulated_commissioning import SimulatedCommissioning
 from pySC.core.constants import NUM_TO_AB, RF_PROPERTIES
@@ -8,6 +8,97 @@ from pySC.utils import at_wrapper, logging_tools
 
 LOGGER = logging_tools.get_logger(__name__)
 
+def SCgetModelRM_mod(SC, BPMords, CMords, trackMode='TBT', Z0=np.zeros(6), nTurns=1, dkick=1e-5, useIdealRing=True):
+    """
+    Determine the lattice response matrix based on current setpoints.
+
+    SCgetModelRM calculates the response matrix `RM` with the BPMs at the ordinates `BPMords`
+    and corrector magnets at ordinates `CMords` using the current magnet setpoints without any
+    roll/alignment/calibration errors. `CMords` is a 2-cell array containing the two lists for the
+    horizontal and vertical CMs respectively.
+    This routine can determine the turn-by-turn RM, as well as the orbit-RM; see option 'trackMode'.
+
+    Args:
+        SC:
+            SimulatedCommissioning class instance
+        BPMords:
+            Index of BPMs in SC.RING (SC.ORD.BPM)
+        CMords:
+            Index of Correctors Magnets in SC.RING (SC.ORD.CM)
+        trackMode:
+            (default = 'TBT') If `TBT` the turn-by-turn RM is calculated.
+            If `ORB` the orbit RM is calculated, using `at.findorbit6`
+        Z0:
+            (default = numpy.zeros(6)) Initial condition for tracking.
+            In `ORB`-mode this is used as the initial guess for `findorbit6`.
+        nTurns:
+            (default = 1) Number of turns over which to determine the TBT-RM. Ignored if in `ORB`-mode.
+        dkick:
+            (default = 1e-5) Kick [rad] to be added when numerically determining the partial derivatives.
+        useIdealRing:
+            (default = True) If True, the design lattice specified in `SC.IDEALRING` is used.
+            If False, the model lattice is used SCgetModelRING(SC).
+
+    Returns:
+        The response matrix given in [m/rad].
+
+    Examples:
+        Compute a response matrix::
+
+            RM1 = SCgetModelRM(SC, SC.ORD.BPM, SC.ORD.CM, nTurns=1)
+
+    """
+    LOGGER.info('Calculating model response matrix')
+    track_methods = dict(TBT=at_wrapper.lattice_track, ORB=orbpass)
+    if trackMode not in track_methods.keys():
+        ValueError(f'Unknown track mode {trackMode}. Valid values are {track_methods.keys()}')
+    ring = SC.IDEALRING.deepcopy() if useIdealRing else SCgetModelRING(SC)
+    trackmethod = track_methods[trackMode]
+    if trackMode == 'ORB':
+        nTurns = 1
+    nBPM = len(BPMords)
+    nCM = len(CMords[0]) + len(CMords[1])
+    RM = np.full((2 * nBPM * nTurns, nCM), np.nan)
+    Ta = trackmethod(ring, Z0,  nTurns, BPMords)
+
+    if np.any(np.isnan(Ta)):
+        raise ValueError('Initial trajectory/orbit is NaN. Aborting. ')
+
+    cnt = 0
+    for nDim in range(2):  # 0: Horizontal, 1: Vertical
+        for j, CMord in enumerate(CMords[nDim]):
+
+
+            this_dkick = dkick[nDim][j] if isinstance(dkick, (list, tuple, np.ndarray)) else dkick
+
+            if ring[CMord].PassMethod == 'CorrectorPass':
+
+                KickNominal = ring[CMord].KickAngle[nDim]
+
+                ring[CMord].KickAngle[nDim] = KickNominal + this_dkick
+
+
+                TdB = trackmethod(ring, Z0, nTurns, BPMords)
+                ring[CMord].KickAngle[nDim] = KickNominal
+            else:
+                PolynomNominal = getattr(ring[CMord], f"Polynom{NUM_TO_AB[nDim]}")
+                delta = this_dkick / ring[CMord].Length
+                changed_polynom = copy.deepcopy(PolynomNominal[:])
+                changed_polynom[0] += (-1) ** (nDim + 1) * delta
+                setattr(ring[CMord], f"Polynom{NUM_TO_AB[nDim]}", changed_polynom[:])
+                TdB = trackmethod(ring, Z0, nTurns, BPMords)
+                setattr(ring[CMord], f"Polynom{NUM_TO_AB[nDim]}", PolynomNominal[:])
+
+
+            dTdB = (TdB - Ta) #/ this_dkick
+            RM[:, cnt] = np.concatenate((np.ravel(np.transpose(dTdB[0, :, :, :], axes=(2, 1, 0))),
+                                         np.ravel(np.transpose(dTdB[2, :, :, :], axes=(2, 1, 0)))))
+
+
+            cnt += 1
+
+    return RM
+    
 def SCgetModelRM(SC, BPMords, CMords, trackMode='TBT', Z0=np.zeros(6), nTurns=1, dkick=1e-5, useIdealRing=True):
     """
     Determine the lattice response matrix based on current setpoints.
@@ -34,7 +125,7 @@ def SCgetModelRM(SC, BPMords, CMords, trackMode='TBT', Z0=np.zeros(6), nTurns=1,
         nTurns:
             (default = 1) Number of turns over which to determine the TBT-RM. Ignored if in `ORB`-mode.
         dkick:
-            (default = 1e-5) Kick (scalar or array-like inputs) [rad] to be added when numerically determining the partial derivatives.
+            (default = 1e-5) Kick [rad] to be added when numerically determining the partial derivatives.
         useIdealRing:
             (default = True) If True, the design lattice specified in `SC.IDEALRING` is used.
             If False, the model lattice is used SCgetModelRING(SC).
@@ -60,15 +151,24 @@ def SCgetModelRM(SC, BPMords, CMords, trackMode='TBT', Z0=np.zeros(6), nTurns=1,
     nCM = len(CMords[0]) + len(CMords[1])
     RM = np.full((2 * nBPM * nTurns, nCM), np.nan)
     Ta = trackmethod(ring, Z0,  nTurns, BPMords)
+
     if np.any(np.isnan(Ta)):
         raise ValueError('Initial trajectory/orbit is NaN. Aborting. ')
+
     cnt = 0
     for nDim in range(2):  # 0: Horizontal, 1: Vertical
         for j, CMord in enumerate(CMords[nDim]):
+
+
             this_dkick = dkick[nDim][j] if isinstance(dkick, (list, tuple, np.ndarray)) else dkick
+
             if ring[CMord].PassMethod == 'CorrectorPass':
+
                 KickNominal = ring[CMord].KickAngle[nDim]
+
                 ring[CMord].KickAngle[nDim] = KickNominal + this_dkick
+
+
                 TdB = trackmethod(ring, Z0, nTurns, BPMords)
                 ring[CMord].KickAngle[nDim] = KickNominal
             else:
@@ -79,15 +179,54 @@ def SCgetModelRM(SC, BPMords, CMords, trackMode='TBT', Z0=np.zeros(6), nTurns=1,
                 setattr(ring[CMord], f"Polynom{NUM_TO_AB[nDim]}", changed_polynom[:])
                 TdB = trackmethod(ring, Z0, nTurns, BPMords)
                 setattr(ring[CMord], f"Polynom{NUM_TO_AB[nDim]}", PolynomNominal[:])
+
+
             dTdB = (TdB - Ta) / this_dkick
             RM[:, cnt] = np.concatenate((np.ravel(np.transpose(dTdB[0, :, :, :], axes=(2, 1, 0))),
                                          np.ravel(np.transpose(dTdB[2, :, :, :], axes=(2, 1, 0)))))
+      
+
             cnt += 1
+
     return RM
+    
+    
+    
 
-
+import numpy as np
 def orbpass(RING, Z0,  nTurns, REFPTS):
     return np.transpose(at_wrapper.findorbit6(RING, REFPTS)[1])[[0,1,2,3], :].reshape(4, 1, len(REFPTS), 1)
+def SCgetModelRING(SC: SimulatedCommissioning, includeAperture: bool =False) -> Lattice:
+    """
+    Returns a model lattice based on current setpoints
+
+    This function calculates a model lattice based on the setpoints of `SC.RING`. Misalignments,
+    lattice errors and dipole fields are excluded.
+
+    Args:
+        SC: SimulatedCommissioning class instance
+        includeAperture: (default=False) If true, the returned model ring includes the aperture
+
+    Returns:
+        The idealised RING structure
+
+    """
+    ring = SC.IDEALRING.deepcopy()
+    for ord in range(len(SC.RING)):
+        if hasattr(SC.RING[ord], 'SetPointA') and hasattr(SC.RING[ord], 'SetPointB'):
+            ring[ord].PolynomA = SC.RING[ord].SetPointA
+            ring[ord].PolynomB = SC.RING[ord].SetPointB
+            ring[ord].PolynomA[0] = 0.0
+            ring[ord].PolynomB[0] = 0.0
+        if includeAperture:
+            if 'EApertures' in SC.RING[ord]:
+                ring[ord].EApertures = SC.RING[ord].EApertures
+            if 'RApertures' in SC.RING[ord]:
+                ring[ord].RApertures = SC.RING[ord].RApertures
+        if len(SC.ORD.RF) and hasattr(SC.RING[ord], 'Frequency'):
+            for field in RF_PROPERTIES:
+                setattr(ring[ord], field, getattr(SC.RING[ord], f"{field}SetPoint"))
+    return ring
 
 
 def SCgetModelDispersion(SC, BPMords, CAVords, trackMode='ORB', Z0=np.zeros(6), nTurns=1, rfStep=1E3, useIdealRing=True):
@@ -140,36 +279,3 @@ def SCgetModelDispersion(SC, BPMords, CAVords, trackMode='ORB', Z0=np.zeros(6), 
     eta = np.concatenate((np.ravel(np.transpose(dTdB[0, :, :, :], axes=(2, 1, 0))),
                           np.ravel(np.transpose(dTdB[2, :, :, :], axes=(2, 1, 0)))))
     return eta
-
-
-def SCgetModelRING(SC: SimulatedCommissioning, includeAperture: bool =False) -> Lattice:
-    """
-    Returns a model lattice based on current setpoints
-
-    This function calculates a model lattice based on the setpoints of `SC.RING`. Misalignments,
-    lattice errors and dipole fields are excluded.
-
-    Args:
-        SC: SimulatedCommissioning class instance
-        includeAperture: (default=False) If true, the returned model ring includes the aperture
-
-    Returns:
-        The idealised RING structure
-
-    """
-    ring = SC.IDEALRING.deepcopy()
-    for ord in range(len(SC.RING)):
-        if hasattr(SC.RING[ord], 'SetPointA') and hasattr(SC.RING[ord], 'SetPointB'):
-            ring[ord].PolynomA = SC.RING[ord].SetPointA
-            ring[ord].PolynomB = SC.RING[ord].SetPointB
-            ring[ord].PolynomA[0] = 0.0
-            ring[ord].PolynomB[0] = 0.0
-        if includeAperture:
-            if 'EApertures' in SC.RING[ord]:
-                ring[ord].EApertures = SC.RING[ord].EApertures
-            if 'RApertures' in SC.RING[ord]:
-                ring[ord].RApertures = SC.RING[ord].RApertures
-        if len(SC.ORD.RF) and hasattr(SC.RING[ord], 'Frequency'):
-            for field in RF_PROPERTIES:
-                setattr(ring[ord], field, getattr(SC.RING[ord], f"{field}SetPoint"))
-    return ring

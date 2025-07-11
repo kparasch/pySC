3511e2cef1632fdd454a765b2dd0e7fc1fdb0d4e
kparasch <konstantinos.paraschou@desy.de>
2025-07-11 10:27:38 +0200

    rm whitespace

--- pySC/core/new_api.py
+++ pySC/core/new_api.py
@@ -5,16 +5,8 @@ def new_api(SC):
 
     SC.bpm_system = BPMSystem(SC)
     print(SC.bpm_system.rms_errors)
-    
-    #orbits_new = np.zeros([100,2,len(bpms.indices)])
-    #orbits_old = np.zeros([100,2,len(bpms.indices)])
-    #
-    # for ii in range(10):
-    #     orbits_new[ii] = bpms._capture_orbit()
-    #     orbits_old[ii] = bpms.capture_orbit()
-    
-    #gs, ge = SC.ORD.Girder[:,0]
-    
+
+
     SC.supports = SupportSystem(parent=SC)
     for index in SC.ORD.Magnet:
         SC.supports.add_element(index)
@@ -24,13 +16,13 @@ def new_api(SC):
         SC.supports.data['L0'][index].roll = float(SC.RING[index].MagnetRoll[0])
         SC.supports.data['L0'][index].pitch = float(SC.RING[index].MagnetRoll[1])
         SC.supports.data['L0'][index].yaw = float(SC.RING[index].MagnetRoll[2])
-    
+
     for index in SC.ORD.BPM:
         SC.supports.add_element(index)
         SC.supports.data['L0'][index].dx = float(SC.RING[index].Offset[0])
         SC.supports.data['L0'][index].dy = float(SC.RING[index].Offset[1])
         SC.supports.data['L0'][index].roll = float(SC.RING[index].Roll)
-    
+
     for ii in range(SC.ORD.Girder.shape[1]):
         SC.supports.add_support(SC.ORD.Girder[0, ii], SC.ORD.Girder[1, ii], name='Girder', level=1)
         sd = SC.RING[SC.ORD.Girder[0,ii]].GirderOffset

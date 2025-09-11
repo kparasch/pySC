63176e98336f65b6e3e8cbe4dfec15e4f2ec7ce2
kparasch <konstantinos.paraschou@desy.de>
2025-09-11 14:04:10 +0200

    send logging to stdout instead of stderr

--- pySC/__init__.py
+++ pySC/__init__.py
@@ -10,11 +10,13 @@ __version__ = "0.3.0"
 
 from .core.new_simulated_commissioning import SimulatedCommissioning
 import logging
+import sys
 
 logging.basicConfig(
     #format='%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(message)s',
     format="{asctime} | {levelname} | {message}",
     datefmt="%d %b% %Y, %H:%M:%S",
-    level=logging.DEBUG,
-    style='{'
+    level=logging.INFO,
+    style='{',
+    stream=sys.stdout
 )

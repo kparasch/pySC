e49afa6c56f0134e11abcd5d3a9f6c388b9a037c
kparasch <konstantinos.paraschou@desy.de>
2025-10-23 17:54:00 +0200

    first try of 'interactive' bba measurement

---
+++ pySC/tuning/bba.py
@@ -0,0 +1,238 @@
+from pydantic import BaseModel
+from typing import Optional, Callable
+import datetime
+import logging
+import numpy as np
+from ..utils.file_tools import dict_to_h5
+from .tools import hysteresis_loop, get_average_orbit
+
+logger = logging.getLogger(__name__)
+
+class BBAData(BaseModel):
+    """
+    A class to hold the data collected during a BBA measurement.
+    It contains the BPM positions and the orbit data.
+    """
+    quadrupole: str
+    bpm: str
+    corrector: str 
+    plane: str
+    dk0l: float  # Corrector k0 step
+    dk1: float  # Quadrupole k1 step
+    n0: int  # Number of steps in the corrector strength
+    shots_per_orbit: int
+    bipolar: bool = True
+
+    initial_k0l: Optional[float] = None
+    initial_k1: Optional[float] = None
+    timestamp: Optional[float] = None
+
+    raw_bpm_x_center: list[list[float]] = []
+    raw_bpm_y_center: list[list[float]] = []
+    raw_bpm_x_up: list[list[float]] = []
+    raw_bpm_y_up: list[list[float]] = []
+    raw_bpm_x_down: list[list[float]] = []
+    raw_bpm_y_down: list[list[float]] = []
+
+    raw_bpm_x_center_err: list[list[float]] = []
+    raw_bpm_y_center_err: list[list[float]] = []
+    raw_bpm_x_up_err: list[list[float]] = []
+    raw_bpm_y_up_err: list[list[float]] = []
+    raw_bpm_x_down_err: list[list[float]] = []
+    raw_bpm_y_down_err: list[list[float]] = []
+
+    def save(self):
+        dict_to_save = self.model_dump()
+        time_str = datetime.datetime.fromtimestamp(self.timestamp).strftime("%Y%m%d_%H%M%S")
+        filename = f'data/BBA_{self.bpm}_{self.plane}_{time_str}.h5'
+        dict_to_h5(dict_to_save, filename)
+        print(f'Saved data to {filename}')
+
+class BBA_Measurement(BaseModel):
+    """
+    A class to perform a beam-based alignment measurement to find the quadrupole center in a storage ring.
+    It sets the quadrupole and corrector strengths, collects orbit data.
+    """
+    bpm : str
+    quadrupole: str
+    h_corrector: Optional[str]
+    v_corrector: Optional[str]
+    dk0l_x: float
+    dk1_x: float
+    dk0l_y: float
+    dk1_y: float
+    n0: int = 7  # Number of steps in the corrector strength
+    bpm_number: int = 1  # Placeholder for BPM number, can be set later
+    shots_per_orbit: int = 2
+    bipolar: bool = True
+    skip_save: bool = False
+
+    initial_h_k0l: Optional[float] = None
+    initial_v_k0l: Optional[float] = None
+    initial_k1: Optional[float] = None
+
+    H_data: Optional[BBAData] = None
+    V_data: Optional[BBAData] = None
+
+    ## make these private
+    get_orbit: Optional[Callable] = None # to be set at generation of measurement
+    settings: Optional[Callable] = None # to be set at generation of measurement
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Initialize BBAData instances for horizontal and vertical procedures
+        if self.h_corrector is not None:
+            self.H_data = BBAData(plane='X', bpm=self.bpm, quadrupole=self.quadrupole, 
+                                  corrector=self.h_corrector, dk0l=self.dk0l_x, dk1=self.dk1_x,
+                                  n0=self.n0, shots_per_orbit=self.shots_per_orbit, bipolar=self.bipolar)
+        if self.v_corrector is not None:
+            self.V_data = BBAData(plane='Y', bpm=self.bpm, quadrupole=self.quadrupole, 
+                                  corrector=self.v_corrector, dk0l=self.dk0l_y, dk1=self.dk1_y,
+                                  n0=self.n0, shots_per_orbit=self.shots_per_orbit, bipolar=self.bipolar)
+
+
+    def print_init(self):
+        logger.debug("Measurement plan:")
+        logger.debug(f"    BPM = {self.bpm}")
+        logger.debug(f"    Quadrupole = {self.quadrupole}")
+        logger.debug(f"    H. corrector = {self.h_corrector}")
+        logger.debug(f"        dk0l = {self.dk0l_x*1e6:.1f} urad")
+        logger.debug(f"        dk1 = {self.dk1_x:.3f} 1/m^2")
+        logger.debug(f"    V. corrector = {self.v_corrector}")
+        logger.debug(f"        dk0l = {self.dk0l_y*1e6:.1f} urad")
+        logger.debug(f"        dk1 = {self.dk1_y:.3f} 1/m^2")
+        logger.debug(f"    number of points = {self.n0}")
+        logger.debug(f"    Initial H. k0l = {self.initial_h_k0l*1e6:.1f} urad")
+        logger.debug(f"    Initial V. k0l = {self.initial_v_k0l*1e6:.1f} urad")
+        logger.debug(f"    Initial k1 = {self.initial_k1:.3f} 1/m^2")
+        logger.debug(f"    Bipolar = {self.bipolar}")
+        logger.debug("")
+
+    def one_plane_loop(self, plane: str):
+        assert plane in ['H', 'V']
+        if plane == 'H':
+            logger.debug('Starting measurement in horizontal plane.')
+            code = 2
+        else:
+            logger.debug('Starting measurement in vertical plane.')
+            code = 4
+
+        corrector = self.h_corrector if plane == 'H' else self.v_corrector
+        initial_k0l = self.initial_h_k0l if plane == 'H' else self.initial_v_k0l
+        data = self.H_data if plane == 'H' else self.V_data
+
+        logger.debug('Setting corrector to under first value (k0 - 1.2 dk0) for hysteresis')
+        self.settings.set(corrector, initial_k0l - 1.2 * data.dk0l)
+        yield code
+
+        k0_array = np.linspace(-data.dk0l, data.dk0l, self.n0) + initial_k0l
+
+        for ii, k0_sp in enumerate(k0_array):
+
+            # set next setpoint in corrector
+            logger.debug(f'{ii+1}/{self.n0} Stepping to next corrector setpoint: {k0_sp*1e6:+.1f} murad')
+            self.settings.set(corrector, k0_sp)
+            yield code
+
+            # correct vertical orbit?
+
+            logger.debug('    Acquiring orbit data for quad zero')
+            orbit_x, orbit_y, orbit_x_err, orbit_y_err = get_average_orbit(n_orbits=self.shots_per_orbit)
+            data.raw_bpm_x_center.append(list(orbit_x))
+            data.raw_bpm_y_center.append(list(orbit_y))
+            data.raw_bpm_x_center_err.append(list(orbit_x_err))
+            data.raw_bpm_y_center_err.append(list(orbit_y_err))
+
+            # set "up" setpoint in quadrupole
+            logger.debug(f'    Setting quadrupole to up setpoint: {self.initial_k1 + data.dk1} 1/m')
+            self.settings.set(self.quadrupole, self.initial_k1 + data.dk1)
+            yield code
+
+            logger.debug('    Acquiring orbit data for quad up')
+            orbit_x, orbit_y, orbit_x_err, orbit_y_err = get_average_orbit(n_orbits=self.shots_per_orbit)
+            data.raw_bpm_x_up.append(list(orbit_x))
+            data.raw_bpm_y_up.append(list(orbit_y))
+            data.raw_bpm_x_up_err.append(list(orbit_x_err))
+            data.raw_bpm_y_up_err.append(list(orbit_y_err))
+
+            if self.bipolar:
+                # set "down" setpoint in quadrupole
+                logger.debug(f'    Setting quadrupole to down setpoint: {self.initial_k1 - data.dk1}')
+                self.settings.set(self.quadrupole, self.initial_k1 - data.dk1)
+                yield code
+
+                logger.debug('    Acquiring orbit data for quad down')
+                # orbit_x, orbit_y = get_pydoocs_orbit()
+                orbit_x, orbit_y, orbit_x_err, orbit_y_err = get_average_orbit(n_orbits=self.shots_per_orbit)
+                data.raw_bpm_x_down.append(list(orbit_x))
+                data.raw_bpm_y_down.append(list(orbit_y))
+                data.raw_bpm_x_down_err.append(list(orbit_x_err))
+                data.raw_bpm_y_down_err.append(list(orbit_y_err))
+            else:
+                zero_orbit = list(orbit_x*0)
+                data.raw_bpm_x_down.append(zero_orbit)
+                data.raw_bpm_y_down.append(zero_orbit)
+                data.raw_bpm_x_down_err.append(zero_orbit)
+                data.raw_bpm_y_down_err.append(zero_orbit)
+
+
+            # restore quadrupole to initial setpoint
+            logger.debug(f'    Restoring quadrupole to initial setpoint: {self.initial_k1}')
+            self.settings.set(self.quadrupole, self.initial_k1)
+
+            logger.debug("")
+        # restore corrector to initial setpoint
+        self.settings.set(corrector, initial_k0l)
+
+        #save data
+        if not self.skip_save:
+            data.save()
+        yield code+1
+
+    def generate(self, get_orbit, settings):
+        """
+        step through the measurement.
+        """
+        self.get_orbit = get_orbit
+        self.settings = settings
+
+        timestamp = datetime.datetime.now().timestamp()
+        # for restoring at the end
+        self.initial_k1 = settings.get(self.quadrupole)
+        if self.h_corrector is not None:
+            self.initial_h_k0l = settings.get(self.h_corrector)
+            self.H_data.initial_k0l = self.initial_h_k0l
+            self.H_data.initial_k1 = self.initial_k1
+            self.H_data.timestamp = timestamp
+
+        if self.v_corrector is not None:
+            self.initial_v_k0l = settings.get(self.v_corrector)
+            self.V_data.initial_k0l = self.initial_v_k0l
+            self.V_data.initial_k1 = self.initial_k1
+            self.V_data.timestamp = timestamp
+
+        self.print_init()
+
+        if self.h_corrector is None:
+            dk1 = self.V_data.dk1
+        elif self.v_corrector is None:
+            dk1 = self.H_data.dk1
+        else:
+            dk1 = max(self.H_data.dk1, self.V_data.dk1)
+
+        for code in hysteresis_loop(self.quadrupole, settings, dk1, n_cycles=2, bipolar=self.bipolar):
+            yield code
+
+        if self.h_corrector is not None:
+            for code in self.one_plane_loop('H'):
+                yield code
+
+        if self.v_corrector is not None:
+            for code in self.one_plane_loop('V'):
+                yield code
+
+    def run(self, generator=None):
+        if generator is None:
+            generator = self.generate()
+        for code in generator:
+            logger.debug(f'    Got code: {code}')
--- pySC/tuning/tools.py
+++ pySC/tuning/tools.py
@@ -1,10 +1,15 @@
 import numpy as np
 import logging
 from .response_matrix import ResponseMatrix
+from pydantic import BaseModel
+from typing import Optional, Callable
+import datetime
+from ..utils.file_tools import dict_to_h5
+from .bba import BBA_Measurement ## circular import :(
 
 logger = logging.getLogger(__name__)
 
-def orbit_correction(get_orbit, settings, response_matrix: ResponseMatrix, correctors, method='svd_cutoff', parameter=0, reference=None, gain=1, apply=False):
+def orbit_correction(get_orbit: Callable, settings, response_matrix: ResponseMatrix, correctors, method='svd_cutoff', parameter=0, reference=None, gain=1, apply=False):
 
     if not apply and gain != 1:
         logger.warning("Gain is set but apply is False, gain will have no effect.")
@@ -25,3 +30,70 @@ def orbit_correction(get_orbit, settings, response_matrix: ResponseMatrix, corre
         settings.set_many(data)
 
     return trims
+
+def hysteresis_loop(name, settings, delta, n_cycles=1, bipolar=True):
+    sp0 = settings.get(name)
+
+    logger.debug(f'Hysteresis loop started for {name}.')
+    logger.debug('    Going up to (sp0 + delta)')
+
+    for _ in range(n_cycles):
+        settings.set(sp0 + delta)
+        yield 0
+        if bipolar:
+            logger.debug('    Going down to (sp0 - delta)')
+            settings.set(sp0 - delta)
+        else:
+            logger.debug('    Going down to (sp0)')
+            settings.set(sp0)
+        yield 0
+
+    if bipolar:
+        logger.debug('    Going back to (sp0 - delta)')
+        settings.set(sp0)
+    yield 1
+
+
+def get_average_orbit(get_orbit: Callable, n_orbits: int = 10):
+    orbit_x, orbit_y = get_orbit()
+    all_orbit_x = np.zeros((len(orbit_x), n_orbits))
+    all_orbit_y = np.zeros((len(orbit_y), n_orbits))
+
+    all_orbit_x[:,0] = orbit_x
+    all_orbit_y[:,0] = orbit_y
+    for ii in range(1, n_orbits):
+        all_orbit_x[:, ii], all_orbit_y[:, ii] = get_orbit()
+
+    mean_orbit_x = np.mean(all_orbit_x, axis=1)
+    mean_orbit_y = np.mean(all_orbit_y, axis=1)
+    std_orbit_x = np.std(all_orbit_x, axis=1)
+    std_orbit_y = np.std(all_orbit_y, axis=1)
+    return mean_orbit_x, mean_orbit_y, std_orbit_x, std_orbit_y
+
+
+def orbit_bba(get_orbit, settings, bpm_name, plane, config, shots_per_orbit: int = 1,
+              n_corr_steps: int = 7, bipolar: bool = True, skip_save: bool = False):
+
+    measurement = BBA_Measurement(bpm=bpm_name,
+                                  quadrupole=config['QUAD'],
+                                  h_corrector=config['HCORR'],
+                                  v_corrector=config['VCORR'],
+                                  dk0l_x=config['HCORR_delta'],
+                                  dk1_x=config['QUAD_dk_H'],
+                                  dk0l_y=config['VCORR_delta'],
+                                  dk1_y=config['QUAD_dk_V'],
+                                  n0=n_corr_steps,
+                                  bpm_number=config['number'],
+                                  shots_per_orbit=shots_per_orbit,
+                                  bipolar=bipolar,
+                                  skip_save=skip_save
+                                 )
+
+    measurement.generate(get_orbit=get_orbit, settings=settings)
+    measurement.run() # ??
+
+    H_data = measurement.H_data
+    V_data = measurement.V_data
+    ## analyze data here ? 
+    return 
+

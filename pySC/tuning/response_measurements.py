35a42d5bdd1fc466b67c4ee971ff9c43524ff910
kparasch <konstantinos.paraschou@desy.de>
2026-02-09 17:34:24 +0100

    use logger instead of print

--- pySC/tuning/response_measurements.py
+++ pySC/tuning/response_measurements.py
@@ -1,15 +1,19 @@
 from typing import Union, Optional, TYPE_CHECKING
 import numpy as np
+import logging
+
 from .pySC_interface import pySCInjectionInterface, pySCOrbitInterface
 from ..apps import measure_ORM, measure_dispersion
 
 if TYPE_CHECKING:
     from ..core.new_simulated_commissioning import SimulatedCommissioning
 
+logger = logging.getLogger(__name__)
+
 def measure_TrajectoryResponseMatrix(SC: "SimulatedCommissioning", n_turns: int = 1,
                                      dkick: Union[float, list] = 1e-5, use_design: bool = False,
                                      normalize: bool = True, bipolar: bool = False) -> np.ndarray:
-    print('Calculating response matrix')
+    logger.info(f'Measuring trajectory response matrix with {n_turns=}.')
 
     ### set inputs
     HCORR = SC.tuning.HCORR
@@ -36,7 +40,7 @@ def measure_TrajectoryResponseMatrix(SC: "SimulatedCommissioning", n_turns: int 
 def measure_OrbitResponseMatrix(SC: "SimulatedCommissioning", HCORR: Optional[list] = None,
                                 VCORR: Optional[list] = None, dkick: Union[float, list] = 1e-5,
                                 use_design: bool = False, normalize: bool = True, bipolar: bool = True) -> np.ndarray:
-    print('Calculating response matrix')
+    logger.info('Measuring orbit response matrix.')
 
     ### set inputs
     if HCORR is None:
@@ -64,7 +68,7 @@ def measure_OrbitResponseMatrix(SC: "SimulatedCommissioning", HCORR: Optional[li
 
 def measure_RFFrequencyOrbitResponse(SC: "SimulatedCommissioning", delta_frf : float = 20, use_design: bool = False,
                                      normalize: bool = True, bipolar: bool = False) -> np.ndarray:
-
+    logger.info('Measuring orbit response to RF frequency (dispersion).')
     interface = pySCOrbitInterface(SC=SC)
     interface.use_design = use_design
 

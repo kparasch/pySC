c8e0af20fff30061945b550b2cb351bffd3438ec
kparasch <konstantinos.paraschou@desy.de>
2026-02-10 15:44:23 +0100

    single plane bba

--- pySC/apps/measurements.py
+++ pySC/apps/measurements.py
@@ -48,7 +48,9 @@ def orbit_correction(interface: AbstractInterface, response_matrix: ResponseMatr
     return trims
 
 def measure_bba(interface: AbstractInterface, bpm_name, config: dict, shots_per_orbit: int = 1,
-                n_corr_steps: int = 7, bipolar: bool = True, skip_save: bool = False, folder_to_save: Optional[Path] = None) -> Generator:
+                n_corr_steps: int = 7, bipolar: bool = True, skip_save: bool = False,
+                folder_to_save: Optional[Path] = None, plane: Optional[str] = None,
+                skip_cycle: bool = False) -> Generator:
 
     if folder_to_save is None:
         folder_to_save = Path('data')
@@ -73,10 +75,10 @@ def measure_bba(interface: AbstractInterface, bpm_name, config: dict, shots_per_
                                   n0=n_corr_steps,
                                   bpm_number=config['number'],
                                   shots_per_orbit=shots_per_orbit,
-                                  bipolar=bipolar,
+                                  bipolar=bipolar
                                  )
 
-    generator = measurement.generate(interface=interface)
+    generator = measurement.generate(interface=interface, plane=plane)
 
     # run measurement loop
     for code in generator:

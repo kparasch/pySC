cd1448736039ed9add96cbbbb72fa8ef72302c45
kparasch <konstantinos.paraschou@desy.de>
2026-05-18 15:54:27 +0200

    tuning explanations

--- examples/01_hmba_response_matrices.py
+++ examples/01_hmba_response_matrices.py
@@ -1,5 +1,6 @@
 """Generate HMBA model response matrices."""
 
+import os
 from pathlib import Path
 
 examples_path = Path(__file__).resolve().parents[0]
@@ -7,7 +8,10 @@ examples_path = Path(__file__).resolve().parents[0]
 if __name__ == "__main__":
     from pySC import generate_SC
 
-    sc = generate_SC(examples_path / Path("hmba_config.yaml"), seed=1, sigma_truncate=3)
-    sc.tuning.calculate_model_orbit_response_matrix(save_as=examples_path / Path("output/hmba_ideal_orm.json"))
+    os.chdir(examples_path)
+    Path("output").mkdir(exist_ok=True)
+
+    sc = generate_SC("hmba_config.yaml", seed=1, sigma_truncate=3)
+    sc.tuning.calculate_model_orbit_response_matrix(save_as="output/hmba_ideal_orm.json")
     sc.tuning.calculate_model_trajectory_response_matrix(n_turns=1, save_as="output/hmba_ideal_1turn_orm.json")
     sc.tuning.calculate_model_trajectory_response_matrix(n_turns=2, save_as="output/hmba_ideal_2turn_orm.json")
